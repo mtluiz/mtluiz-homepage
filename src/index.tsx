87251e1a4dd2048e70a12a319b8c6fa32830803e
Matheus Madureira <matheusluizn@gmail.com>
2022-04-07 05:26:40 -0300

    feat: import scss stylesheet

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import './scss/index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
