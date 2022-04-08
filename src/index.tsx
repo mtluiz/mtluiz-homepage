4a8e0670fa25d366d5dd2237e1ab811ef013fdc2
Matheus Madureira <matheusluizn@gmail.com>
2022-04-08 05:15:44 -0300

    feat: switching to createRoot from React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 import './scss/index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container)
+root.render(<React.StrictMode>
+  <App />
+</React.StrictMode>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
+
+
