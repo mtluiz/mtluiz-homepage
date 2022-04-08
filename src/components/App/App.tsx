1be290d8624e45d0775949cbd85772b624fa850e
Matheus Madureira <matheusluizn@gmail.com>
2022-04-08 05:16:17 -0300

    feat: creating home page design concepts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,29 @@
 import { useRef } from "react";
 import * as THREE from "three";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import js from '../../assets/images/javascript.svg';
+
 
 function Box(props: JSX.IntrinsicElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!);
   useFrame((state, delta) => (ref.current.rotation.x += 0.1));
   return (
     <mesh {...props} ref={ref}>
-      <icosahedronGeometry args={[1, 0]} />
-      <meshStandardMaterial color="#ffffff" />
+      <icosahedronGeometry args={[1.5, 0]} />
+      <meshPhysicalMaterial
+        color="#000000"
+        ior={1.5}
+        envMapIntensity={1.5}
+        clearcoat={1}
+        clearcoatNormalScale={new THREE.Vector2(0.2)}
+        metalness={0.2}
+        thickness={2.5}
+        roughness={0.2}
+        transmission={0.9}
+        reflectivity={0.7}
+        clearcoatRoughness={0.1}
+        normalScale={new THREE.Vector2(0.3)}
+      />
     </mesh>
   );
 }
@@ -17,7 +32,7 @@ function App() {
   return (
     <div className="portfolio">
       <header className="portfolio-menu">
-        <div>
+        <div className="begin">
           <h2>mt.</h2>
           <div className="">
             <p>EN</p>
@@ -34,6 +49,17 @@ function App() {
           </div>
         </nav>
       </header>
+
+      <div className="grid-main">
+
+        <img src={js} alt="" width={150} style={{marginRight: '330px'}} />
+
+        <div className="grid">
+          {new Array(32).fill(0).map((_, i) => (
+            <div key={i} className="grid-item"></div>
+          ))}
+        </div>
+      </div>
 
       <main className="portfolio-header section">
         <div className="portfolio-header__name">
@@ -59,10 +85,7 @@ function App() {
 
       <div className="screen">
         <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+          
         </Canvas>
       </div>
     </div>
