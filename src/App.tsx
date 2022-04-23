6dbbee8bc07ff3dfd60af7d40b45bf6a7f028f22
Matheus Madureira <matheusluizn@gmail.com>
2022-04-23 19:13:53 -0300

    chore: organizing project

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import Welcome from "./components/Welcome";
 import Works from "./components/Works";
 import beginHorizontalScroll from "./utils/horizontal-scroll";
 
+import Loading from "./components/Loading";
+
 function App() {
 
   beginHorizontalScroll();
 
   return (
     <>
-    <Welcome />
-    <Works />
+    <Loading />
     </>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,14 @@
+import gsap from "gsap"
+
+export default function Loading() {
+  return (
+    <div className='loading'>
+
+        <h1>Loading</h1>
+
+        <div className="ball"></div>
+
+        <div className="noise"></div>
+    </div>
+  )
+}
