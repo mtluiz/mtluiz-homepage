2478b1f1c89ce8640e1106b3a55b3f7154291ea1
Matheus Madureira <matheusluizn@gmail.com>
2022-05-12 02:08:01 -0300

    feat: creating contact page

---
+++ src/components/Contact.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className="portfolio-works container">
+
+
+          <div className="main">
+              <h1>Contact page</h1>
+              <h1>COMING SOON</h1>
+          </div>
+
+      </div>
+  )
+}
