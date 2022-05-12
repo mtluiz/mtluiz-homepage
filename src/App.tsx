7c9f3bc46fb73093f39543659ce1d8c4574a4914
Matheus Madureira <matheusluizn@gmail.com>
2022-05-11 23:39:17 -0300

    chore: making config changes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,43 @@
 import Welcome from "./components/Welcome";
 import Works from "./components/Works";
 import beginHorizontalScroll from "./utils/horizontal-scroll";
+import ASScroll from "@ashthornton/asscroll";
 
 import Loading from "./components/Loading";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [render, setRender] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [render, setRender] = useState(false); 
 
-  beginHorizontalScroll();
-
-  function next(){
-    setRender(true)
+  function next() {
+    setRender(true);
   }
 
-  useEffect(()=>{
-    setTimeout(()=>{
-      setLoading(!loading)
-    }, 6000)
-  })
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(!loading);
+    }, 6000);
+  });
 
   return (
     <>
-    {render? (<><Welcome /><Works /></>) : <Loading run={loading} next={next} /> }
-    
+      {render ? (
+        <>
+          <Welcome />
+          <div className="sep">
+            <p>Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO</p>
+            <p>Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO</p>
+          </div>
+          <Works />
+          <div className="sep">
+            <p>Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO</p>
+            <p>Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO   Matheus Luiz   2021 PORTFOLIO</p>
+          </div>
+        </>
+      ) : (
+        <Loading run={loading} next={next} />
+      )}
     </>
   );
 }
