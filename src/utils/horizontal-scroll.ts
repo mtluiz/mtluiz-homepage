533add8af0186bb1078d0d7251bdb5cbd1de4e6e
Matheus Madureira <matheusluizn@gmail.com>
2022-05-11 23:37:20 -0300

    feat: creating horizoontal scroll function

--- src/utils/horizontal-scroll.ts
+++ src/utils/horizontal-scroll.ts
@@ -2,5 +2,10 @@ export default function beginHorizontalScroll(){
     window.addEventListener('wheel', (event)=>{
         event.preventDefault();
         window.scroll(window.scrollX + event.deltaY, 0)
+
+        const teste = document.querySelector('.portfolio-works') as HTMLElement
+        console.log('position works:',teste.offsetLeft)
+        console.log('window:',window.scrollX)
+
   }, {passive: false});
 }
