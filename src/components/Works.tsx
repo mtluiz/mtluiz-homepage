1d4d4f69b9b9817628ce22d7529e8df5109a5f55
Matheus Madureira <matheusluizn@gmail.com>
2022-05-11 23:37:56 -0300

    refactor: removing separator from works

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,37 +1,17 @@
 function Works() {
   return (
-    <div className="portfolio-works container">
-      <div className="separator">
-        <div className="separator-top">
-          <p className="separator__text">
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-          </p>
-        </div>
-        <div className="separator-bottom">
-          <p className="separator__text">
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-            <span>MATHEUS LUIZ</span>
-          </p>
-        </div>
+    <>
+      <div className="portfolio-works container">
+
+
+          <div className="main">
+              <h1>PROJECTS &</h1>
+              <h1>WORKS</h1>
+          </div>
+
       </div>
-    </div>
+    </>
   );
 }
 
-
-export default Works; 
+export default Works;
