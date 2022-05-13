8d2f498c85daeb7b8923fbae24ade8ea197b0984
Matheus Madureira <matheusluizn@gmail.com>
2022-05-13 09:40:26 -0300

    feat: creating sphere webgl shader

--- src/3D-objects/Sphere.tsx
+++ src/3D-objects/Sphere.tsx
@@ -3,32 +3,36 @@ import { RefObject, useRef } from "react";
 import * as THREE from "three";
 import { Mesh } from "three";
 
-import vertex from './shaders/vertex.glsl';
-import fragment from './shaders/fragment.glsl';
-
+import vertex from "./shaders/vertex.glsl";
+import fragment from "./shaders/fragment.glsl";
 
 function Sphere(props: JSX.IntrinsicElements["mesh"]) {
+  console.log(vertex);
 
-  const sphere = useRef(null) as RefObject<Mesh>
+  const sphere = useRef(null) as RefObject<Mesh>;
 
-  const mouse =  {x: 0, y: 0}
+  const mouse = { x: 0, y: 0 };
 
-  window.addEventListener('mousemove', (e)=>{
-      mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-      mouse.y = ( e.clientY / window.innerHeight ) * 2 + 1;
-  })
+  window.addEventListener("mousemove", (e) => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = (e.clientY / window.innerHeight) * 2 + 1;
+  });
 
-  useFrame(()=>{
+  useFrame(() => {
     sphere.current!.rotation.y += mouse.y * 0.009;
     sphere.current!.rotation.x += mouse.x * 0.009;
-    sphere.current!.rotation.z += (mouse.x + 0.01) * 0.01 / (mouse.y + 0.01) * 0.01 ;
-  
-  })
+    sphere.current!.rotation.z +=
+      (((mouse.x + 0.01) * 0.01) / (mouse.y + 0.01)) * 0.01;
+  });
 
   return (
-    <mesh {...props} ref={sphere} >
+    <mesh {...props} ref={sphere}>
       <sphereBufferGeometry args={[2, 20, 20]} />
-      <shaderMaterial wireframe={true} ></shaderMaterial>
+      <shaderMaterial
+        wireframe={true}
+        vertexShader={vertex}
+        fragmentShader={fragment}
+      ></shaderMaterial>
     </mesh>
   );
 }
