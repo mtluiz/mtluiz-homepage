233097aba86c3e9204fdfaef9ab4f60c2b8ca77c
mt <matheusluizn@gmail.com>
2022-06-17 06:20:51 -0300

    feat: removing unused log

--- src/main.js
+++ src/main.js
@@ -1,24 +1,19 @@
 import Vaporwave from "./Vaporwave";
 import imageReveal from "./imageReveal";
 import ASScroll from "@ashthornton/asscroll";
+
 import "../scss/index.scss";
 
 const canvas = document.getElementById("screen");
 Vaporwave(canvas);
 
 const asscroll = new ASScroll();
-
-console.log(asscroll)
-
 window.addEventListener("load", () => {
   asscroll.enable({ horizontalScroll: true });
 }, { passive: true });
 
 
-//Start image reveal
-
 const revealElements = document.querySelectorAll("[data-reveal-image]");
-
 Array.from(revealElements).forEach(el => {
   imageReveal(el)
 })
