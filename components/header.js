c42b662f54eb8b12041885e176b875619bee1a89
mt <matheusluizn@gmail.com>
2022-07-07 14:13:38 -0300

    feat: creating header style

--- components/header.js
+++ components/header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Sun from "pixelarticons/svg/sun.svg";
+import Bed from "pixelarticons/svg/bed.svg";
+import github from "../public/images/github-icon.gif";
 import Image from 'next/image';
-import { useColorMode, Box, Container, Heading, Stack, Link, Flex, Button } from "@chakra-ui/react";
-
+import { useColorMode, Box, Heading, Stack, Link, Flex, Button } from "@chakra-ui/react";
 
 const LinkItem = ({ children, href }) => (
   <Link
@@ -14,14 +15,12 @@ const LinkItem = ({ children, href }) => (
 );
 
 export default function Header() {
-
   const { colorMode, toggleColorMode } = useColorMode();
-
   return (
     <Box
-      position={"fixed"}
       as="nav"
       w="100%"
+      maxW={1320}
       height={"100vh"}
       css={{ backdropFilter: "blur(10px)" }}
       p={12}
@@ -56,12 +55,23 @@ export default function Header() {
 
         <Flex>
 
-          <Button colorScheme={"gray"} variant={"solid"} mr={12} rightIcon={<img src="https://www.piskelapp.com/static/resources/home/features/feature-open-source@2x.gif" width={40} height={40} alt="" />}>
+          <Button
+            colorScheme={"gray"}
+            variant={"solid"} mr={12}
+            rightIcon={
+              <Image
+                src={github}
+                width={30}
+                height={30}
+                alt=""
+              />
+            }
+          >
             source
           </Button>
 
-          <Button colorScheme={"yellow"} onClick={toggleColorMode}>
-            <Image src={Sun} width={38} height={38} alt="" />
+          <Button colorScheme={colorMode == "light" ? "blue" : "yellow"} onClick={toggleColorMode}>
+            <Image src={colorMode == "light" ? Bed : Sun} width={38} height={38} alt="" />
           </Button>
 
         </Flex>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import Loader from "../components/loader";
 import Header from "../components/header";
 import Head from "next/head";
+import { Flex, Container, Box } from "@chakra-ui/react";
 
 
 export default function Home() {
   return (
-    <>
+    <Flex
+      direction={"column"}
+      alignItems={"center"}
+    >
       <Header />
-    </>
+      <Box
+        as={"main"}
+        maxW={1320}
+        w={"100%"}
+      >
+        <h1>Conteudo</h1>
+      </Box>
+    </Flex>
   );
 }
