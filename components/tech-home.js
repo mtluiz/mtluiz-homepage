e2825210e5f70a1cb8bb91e149823e8c69fd9860
mt <matheusluizn@gmail.com>
2022-07-08 13:53:08 -0300

    feat: starting 3d home background

---
+++ components/tech-home.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef, useState, useLayoutEffect } from "react";
+import * as THREE from "three";
+
+function createStars(quantity, objectsDistance) {
+  const positions = new Float32Array(quantity * 3);
+
+  for (let i = 0; i < quantity; i++) {
+    positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * 10;
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
+  }
+
+  const particlesGeometry = new THREE.BufferGeometry();
+  particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  const particlesMaterial = new THREE.PointsMaterial({
+    color: '#ffffff',
+    sizeAttenuation: true,
+    size: 0.03
+  });
+  const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+  return particles;
+}
+
+
+export default function TechHome() {
+
+  const screen = useRef(null);
+
+  const [renderer, setRenderer] = useState(null);
+  const [scene, setScene] = useState(null);
+  const [camera, setCamera] = useState(null);
+
+  useEffect(() => {
+    const { current: container } = screen;
+    const renderer = new THREE.WebGLRenderer({ canvas: container, antialias: true, alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    setRenderer(renderer);
+
+    const scene = new THREE.Scene();
+    setScene(scene);
+
+    const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+    setCamera(camera);
+
+    const geom = new THREE.BoxGeometry(1, 1, 1);
+    const text = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geom, text);
+    cube.position.x = 3;
+    camera.position.z = 5;
+    scene.add(cube);
+
+    const stars = createStars(800, 14);
+    scene.add(stars);
+
+
+    function animate() {
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+
+    function handleResize() {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize, false);
+  }, []);
+
+  return (
+    <canvas id="home-screen" ref={screen}></canvas>
+  );
+}
