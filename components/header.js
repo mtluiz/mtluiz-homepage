d17c8aafdf405ab8cef8bf43c67ea84e15b92259
mt <matheusluizn@gmail.com>
2022-07-07 23:57:49 -0300

    feat: creating mobile header styles

--- components/header.js
+++ components/header.js
@@ -1,82 +1,102 @@
-import React from 'react';
+import React from "react";
 import Sun from "pixelarticons/svg/sun.svg";
 import Bed from "pixelarticons/svg/bed.svg";
+import Burger from "pixelarticons/svg/backburger.svg";
 import github from "../public/images/github-icon.gif";
-import Image from 'next/image';
-import { useColorMode, Box, Heading, Stack, Link, Flex, Button } from "@chakra-ui/react";
+import Image from "next/image";
+import {
+  useColorMode,
+  useMediaQuery,
+  Box,
+  Heading,
+  Stack,
+  Link,
+  Flex,
+  Button,
+} from "@chakra-ui/react";
 
 const LinkItem = ({ children, href }) => (
-  <Link
-    href={href}
-    mx={12}
-  >
+  <Link href={href} mx={12}>
     {children}
   </Link>
 );
 
 export default function Header() {
   const { colorMode, toggleColorMode } = useColorMode();
+  const [isMobile] = useMediaQuery("(min-width: 770px)");
+
   return (
-    <Box
-      as="nav"
-      w="100%"
-      maxW={1320}
-      height={"100vh"}
-      css={{ backdropFilter: "blur(10px)" }}
-      p={12}
-    >
+    <>
       <Box
-        display={"flex"}
-        wrap="wrap"
-        align="center"
-        justify="space-between"
+        as="nav"
+        w="100%"
+        maxW={1320}
+        height={"100vh"}
+        css={{ backdropFilter: "blur(8px)" }}
+        p={12}
       >
-        <Heading fontWeight={200} fontSize={"3xl"}>mt</Heading>
-        <Stack
-          direction={{ base: 'column', md: 'row' }}
-          display={{ base: 'none', md: 'flex' }}
-          width={{ base: 'full', md: 'auto' }}
-          alignItems="center"
-          flexGrow={1}
-          mt={{ base: 4, md: 0 }}
+        <Box display={"flex"} wrap="wrap" alignItems="center" w={"100%"} justifyContent="space-between">
+          <Heading fontWeight={200} fontSize={"3xl"}>
+            mt
+          </Heading>
+
+          {!isMobile && <Image src={Burger} alt="" width={50} height={50} />}
+          {isMobile && (
+            <>
+              <Stack
+                direction={{ base: "column", md: "row" }}
+                display={{ base: "none", md: "flex" }}
+                width={{ base: "full", md: "auto" }}
+                alignItems="center"
+                flexGrow={1}
+                mt={{ base: 4, md: 0 }}
+              >
+                <LinkItem href="/about">/works</LinkItem>
+
+                <LinkItem href="/about">/posts</LinkItem>
+              </Stack>
+
+              <Flex>
+                <Button
+                  colorScheme={"gray"}
+                  variant={"solid"}
+                  mr={12}
+                  rightIcon={<Image src={github} width={30} height={30} alt="" />}
+                >
+                  source
+                </Button>
+
+                <Button
+                  colorScheme={colorMode == "light" ? "blue" : "yellow"}
+                  onClick={toggleColorMode}
+                >
+                  <Image
+                    src={colorMode == "light" ? Bed : Sun}
+                    width={38}
+                    height={38}
+                    alt=""
+                  />
+                </Button>
+              </Flex>
+            </>
+          )}
+        </Box>
+        <Box
+          height={"100%"}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"flex-start"}
         >
-          <LinkItem
-            href="/about"
-          >
-            /works
-          </LinkItem>
-
-          <LinkItem
-            href="/about"
-          >
-            /posts
-          </LinkItem>
-        </Stack>
-
-        <Flex>
-
-          <Button
-            colorScheme={"gray"}
-            variant={"solid"} mr={12}
-            rightIcon={
-              <Image
-                src={github}
-                width={30}
-                height={30}
-                alt=""
-              />
-            }
-          >
-            source
-          </Button>
-
-          <Button colorScheme={colorMode == "light" ? "blue" : "yellow"} onClick={toggleColorMode}>
-            <Image src={colorMode == "light" ? Bed : Sun} width={38} height={38} alt="" />
-          </Button>
-
-        </Flex>
-
+          <Heading as={"h1"} >Hi, I&apos;m Matheus</Heading>
+          <Heading as={"h1"} >
+            A Full Stack Developer
+          </Heading>
+          <Heading as={"h1"} >
+            Based in Brazil
+          </Heading>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,15 @@ export default function Home() {
         maxW={1320}
         w={"100%"}
       >
-        <h1>Conteudo</h1>
+        <h1>About Me</h1>
+        <p>
+          Takuya is a freelance and a full-stack developer based in Osaka with a passion for
+          building digital services/stuff he wants. He has a knack for all things launching products,
+          from planning and designing all the way to solving real-life problems with code. When not online, h
+          e loves hanging out with his camera. Currently, he is living off of his own product called Inkdrop.
+          He publishes content for marketing his products and his YouTube channel called Dev as Life
+          has more than 100k subscribers.
+        </p>
       </Box>
     </Flex>
   );
