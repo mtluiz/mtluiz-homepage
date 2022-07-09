a42c9e90edb2cd54ac4b5a680adea2ef25980071
mt <matheusluizn@gmail.com>
2022-07-09 16:21:10 -0300

    feat: creating about me section

---
+++ components/about-me.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Heading, Text, Container, Box, Link } from "@chakra-ui/react";
+
+export default function AboutMe() {
+  return (
+    <Container
+      mt={4}
+    >
+      <Heading
+        fontWeight={100}
+        my={4}
+
+      >
+        Work
+      </Heading>
+
+      <Text
+        my={4}
+      >
+        Matheus is a javascript enthusiast with a passion for building digital services/stuff he wants.
+        <br />
+        <br />
+        He&apos;s obsessed with all things web, from planning and designing all the way to solving real-life problems.
+        When not at work, he loves playing some video games and trying out experimental technologies.
+        <br />
+        <br />
+        Currently, he is working for a company called Globo in the team that maintains the biggest news website in Brasil, <Link href="https://globo.com">globo.com</Link>.
+        <br />
+        <br />
+        He also does some freelance work related to the web, like CMS&apos;s and web apps.
+      </Text>
+    </Container>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Loader from "../components/loader";
 import Header from "../components/header";
+import AboutMe from "../components/about-me";
 import Head from "next/head";
 import { Flex, Container, Box } from "@chakra-ui/react";
 
@@ -16,15 +17,8 @@ export default function Home() {
         maxW={1320}
         w={"100%"}
       >
-        <h1>About Me</h1>
-        <p>
-          Takuya is a freelance and a full-stack developer based in Osaka with a passion for
-          building digital services/stuff he wants. He has a knack for all things launching products,
-          from planning and designing all the way to solving real-life problems with code. When not online, h
-          e loves hanging out with his camera. Currently, he is living off of his own product called Inkdrop.
-          He publishes content for marketing his products and his YouTube channel called Dev as Life
-          has more than 100k subscribers.
-        </p>
+        <AboutMe></AboutMe>
+
       </Box>
     </Flex>
   );
