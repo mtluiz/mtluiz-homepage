66e38da6f5c1007dbed2e0fe7f635285fee6e9eb
mt <matheusluizn@gmail.com>
2022-07-16 16:08:52 -0300

    feat: adding portfolio cta and removing image

--- components/about-me.js
+++ components/about-me.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, Text, Container, Box, Link, Flex, Image } from "@chakra-ui/react";
+import { Heading, Text, Container, Box, Link, Flex, Image, Button } from "@chakra-ui/react";
 
 export default function AboutMe() {
   return (
@@ -27,6 +27,14 @@ export default function AboutMe() {
           <br />
           Currently he is working for a company called Globo, in the team that maintains one of the biggest news websites in Brasil, <Link href="https://globo.com">globo.com</Link>.
         </Text>
+
+        <Button display={"block"} width={"300px"} margin={"0 auto"} colorScheme={"messenger"}>
+          <Heading display={"flex"} justifyContent={"center"} alignItems={"center"} variant={"h6"} fontSize="lg">
+            Check out my portfolio
+            <svg fill="none" width={30} height={30} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"> <path d="M8 5v2h2V5H8zm4 4V7h-2v2h2zm2 2V9h-2v2h2zm0 2h2v-2h-2v2zm-2 2v-2h2v2h-2zm0 0h-2v2h2v-2zm-4 4v-2h2v2H8z" fill="currentColor" /> </svg>
+
+          </Heading>
+        </Button>
       </Container>
 
       <Container>
