a89134db81fc12026543e95205446d88de9ee7f0
mt <matheusluizn@gmail.com>
2022-07-29 01:48:22 -0300

    feat: creating skills section

--- components/skills.js
+++ components/skills.js
@@ -1,25 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { Box, Flex, Heading, Text } from '@chakra-ui/react';
+import { Parallax } from 'react-scroll-parallax';
+import ScrollWithMe from "../lib/scroll-with-me";
 
 export default function Skills() {
   const scroll = useRef(null);
   const owner = useRef(null);
   const [pixels, setPixels] = useState(0);
 
-
-
   useEffect(() => {
-
-    window.addEventListener("scroll", (e) => {
-      const values = scroll.current.getBoundingClientRect();
-      if (window.scrollY > values.y && window.scrollY < 1050) {
-        setPixels(window.scrollY - 800);
-      } else {
-        setPixels(597);
-      }
-
+    new ScrollWithMe(scroll.current, {
+      startAt: 909,
+      endAt: 1600,
+      setTop: 100
     });
-
   }, []);
 
   return (
@@ -31,13 +25,16 @@ export default function Skills() {
       alignItems={"flex-start"}
       ref={owner}
     >
-      <Box ref={scroll} w={"60%"} h={"100%"} transform={`translate(0px, ${pixels}px);`}>
-        <Heading fontSize={"7xl"}>SKILLS</Heading>
-        <Text maxW={"600px"}>
-          SUKU allows users to engage and earn across our and our partners platforms.
-          It connects web3 participants, digital assets, and protocols while enabling use cases and delivering value for the entire ecosystem.
-          Through gamification mechanics, SUKU provides access and unlocks exclusive experiences in the Metaverse.
-        </Text>
+      <Box position={"relative"} w={"60%"} h={"100%"}>
+
+        <Box ref={scroll}>
+          <Heading fontSize={"7xl"}>SKILLS</Heading>
+          <Text maxW={"600px"}>
+            SUKU allows users to engage and earn across our and our partners platforms.
+            It connects web3 participants, digital assets, and protocols while enabling use cases and delivering value for the entire ecosystem.
+            Through gamification mechanics, SUKU provides access and unlocks exclusive experiences in the Metaverse.
+          </Text>
+        </Box>
       </Box>
       <Box w={"40%"}>
         <Box>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "../lib/theme";
 import "../style/index.css";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
-      <Component {...pageProps} />
+      <ParallaxProvider>
+        <Component {...pageProps} />
+      </ParallaxProvider>
     </ChakraProvider>
   );
 }
