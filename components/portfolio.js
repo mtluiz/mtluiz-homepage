0daf6d157b4f8b0550bda061a5f4f9b071160e91
mt <matheusluizn@gmail.com>
2022-08-02 15:03:18 -0300

    feat: portfolio responsive

--- components/portfolio.js
+++ components/portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, Text, Box, Flex } from "@chakra-ui/react";
+import { Heading, Text, Box, Flex, useMediaQuery, useQuery } from "@chakra-ui/react";
 import { Highlight } from '@chakra-ui/react';
 
 const experiences = [
@@ -21,6 +21,7 @@ const experiences = [
 ];
 
 export default function Portfolio() {
+  const [isMobile] = useMediaQuery("(max-width: 770px)");
   return (
     <Box
       minH={"100vh"}
@@ -41,26 +42,42 @@ export default function Portfolio() {
         </Heading>
       </Box>
 
-      <Flex flexDir={"column"} justifyContent="center" alignItems={"center"} background={"#ffffff"} opacity={"1"} boxShadow="rgb(73 73 73 / 20%) 0px 50px 250px" p={"60px"} my={"6vw"} mx={"15vw"} borderRadius={"50px"}>
+      <Flex
+        flexDir={"column"}
+        justifyContent="center"
+        alignItems={"center"}
+        background={"#ffffff"}
+        opacity={"1"}
+        boxShadow="rgb(73 73 73 / 20%) 0px 50px 250px"
+        p={isMobile ? "10px" : "60px"}
+        my={"6vw"}
+        mx={isMobile ? "10px" : "15vw"}
+        borderRadius={"50px"}
+      >
         <div className='square-separator'>
           <div className='square'></div>
           <div className="dotted"></div>
         </div>
         <Heading fontSize={"5xl"} >MY PORTFOLIO</Heading>
-        <Text fontSize={26} textAlign="center" my={10} maxW="400px">
-          <Highlight query='Globo' styles={{ px: '1', py: '1', bg: 'blue.100' }}>
+        <Text fontSize={isMobile ? "2xl" : "3xl"} textAlign="center" my={10} maxW="400px">
+          <Highlight query='Globo' styles={{ px: '1', py: '1', bg: '#2FDAF1' }}>
             Currently he works for the biggest media company in Brazil, Globo.
             He also does some freelance work on his free time.
             Checkout some products he participated/worked on:
           </Highlight>
         </Text>
 
-        <Flex mt={"80px"} w={"100%"} justifyContent="space-evenly">
+        <Flex
+          mt={isMobile ? "20px" : "80px"} w={"100%"}
+          flexDir={isMobile ? "column" : "row"}
+          justifyContent="space-evenly"
+          mb={isMobile ? "40px" : 0}
+        >
           {experiences.map(({ title, content, link }, i) => (
-            <Box key={i}>
+            <Box key={i} my={isMobile ? "20px" : "0"} borderBottom={isMobile ? "1px solid #e6e9e6!important" : ""} pb={isMobile ? "20px" : ""}>
               <a href={link} target="_blank" rel="noopener noreferrer">
-                <Text variant={"h1"} fontWeight={1000}>{title}</Text>
-                <Text fontSize={"12px"} maxW="10rem">
+                <Text pb={isMobile ? "10px" : ""} variant={"h1"} fontWeight={1000}>{title}</Text>
+                <Text fontSize={"12px"} maxW={isMobile ? "auto" : "10rem"}>
                   {content}
                 </Text>
               </a>
