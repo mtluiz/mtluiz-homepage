f3a9fe1a9cfc3bd5d948930c327c8718040e3c78
mt <matheusluizn@gmail.com>
2022-08-05 11:02:31 -0300

    style: add responsive to portfolio

--- components/setup.js
+++ components/setup.js
@@ -8,13 +8,15 @@ export default function Setup() {
       <div className="noise"></div>
       <Heading
         position="absolute"
-        fontSize={"20vw"}
+        fontSize={isMobile ? "60px" : "20vw"}
         textAlign="center"
         whiteSpace="nowrap"
         w="100%"
         zIndex={"-1"}
         opacity={"0.1"}
         top={isMobile ? "6px" : "-10vw"}
+        className="setup__title"
+        left={"0"}
       >
         JS ENTHUSIAST
       </Heading>
