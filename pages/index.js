3c4312e126592e898f6ebfb5db9879bdc0800d3e
mt <matheusluizn@gmail.com>
2022-08-06 18:54:27 -0300

    feat: adding meta tags

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Matheus Luiz - Desenvolvedor Full Stack</title>
+        <title>Matheus Luiz - Full Stack Developer</title>
         <link rel="shortcut icon" href="./favicon.png" />
+        <meta name="title" content="Matheus Luiz - Full Stack Developer" />
+        <meta name="description" content="Matheus is a full stack developer and javascript enthusiast based in Brazil." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://mtluiz.dev/" />
+        <meta property="og:title" content="Matheus Luiz - Full Stack Developer" />
+        <meta property="og:description" content="Matheus is a full stack developer and javascript enthusiast based in Brazil." />
+        <meta property="og:image" content="./images/logo.svg" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://mtluiz.dev/" />
+        <meta property="twitter:title" content="Matheus Luiz - Full Stack Developer" />
+        <meta property="twitter:description" content="Matheus is a full stack developer and javascript enthusiast based in Brazil." />
+        <meta property="twitter:image" content="./images/logo.svg" />
       </Head>
       <main style={{ display: isMobile ? "block" : "flex", justifyContent: isTablet ? "" : "center", alignItems: "center" }}>
         <div style={{ maxWidth: "1400px" }}>
