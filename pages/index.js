a1bc2e525761183a2f0fbe5eb905cfd4fa3e6ac5
mt <matheusluizn@gmail.com>
2022-08-06 18:14:17 -0300

    feat: project index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import Loader from "../components/loader";
 import HomeSection from "../components/home";
 import Setup from "../components/setup";
 import Footer from "../components/footer";
 import Portfolio from "../components/portfolio";
 import Skills from "../components/skills";
-
 import { useMediaQuery } from "@chakra-ui/react";
 
+
 export default function Home() {
+
   const [isMobile] = useMediaQuery("(max-width: 770px)");
   const [isTablet] = useMediaQuery("(max-width: 1200px)");
+
   return (
     <main style={{ display: isMobile ? "block" : "flex", justifyContent: isTablet ? "" : "center", alignItems: "center" }}>
       <div style={{ maxWidth: "1400px" }}>
