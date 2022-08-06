9206c9dec5b56634f5fd13b9f1babdebe0a45666
mt <matheusluizn@gmail.com>
2022-08-06 19:36:03 -0300

    fix: meta tag image

--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,19 @@ export default function Home() {
     <>
       <Head>
         <title>Matheus Luiz - Full Stack Developer</title>
-        <link rel="shortcut icon" href="https://www.mtluiz.dev/images/logo.svg" />
+        <link rel="shortcut icon" href="./favicon.png" />
         <meta name="title" content="Matheus Luiz - Full Stack Developer" />
         <meta name="description" content="Matheus is a full stack developer and javascript enthusiast based in Brazil." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://mtluiz.dev/" />
         <meta property="og:title" content="Matheus Luiz - Full Stack Developer" />
         <meta property="og:description" content="Matheus is a full stack developer and javascript enthusiast based in Brazil." />
-        <meta property="og:image" content="https://www.mtluiz.dev/images/logo.svg" />
+        <meta property="og:image" content="https://www.mtluiz.dev/images/logo-png.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://mtluiz.dev/" />
         <meta property="twitter:title" content="Matheus Luiz - Full Stack Developer" />
         <meta property="twitter:description" content="Matheus is a full stack developer and javascript enthusiast based in Brazil." />
-        <meta property="twitter:image" content="https://www.mtluiz.dev/images/logo.svg" />
+        <meta property="twitter:image" content="https://www.mtluiz.dev/images/logo-png.png" />
       </Head>
       <main style={{ display: isMobile ? "block" : "flex", justifyContent: isTablet ? "" : "center", alignItems: "center" }}>
         <div style={{ maxWidth: "1400px" }}>
